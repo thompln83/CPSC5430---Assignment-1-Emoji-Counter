231b00833d1d662671ca51cd04a621e6f1a07df9
thompln83 <112356921+thompln83@users.noreply.github.com>
2023-10-20 02:56:44 -0500

    Add files via upload

---
+++ emojicounter-assignment1-thompln83-main 2/Assignment1/Assignment1App.swift
@@ -0,0 +1,19 @@
+//
+// Assignment1App.swift : Assignment1
+//
+// Copyright © 2023 Auburn University.
+// All Rights Reserved.
+
+// Lauren Thompson CPSC5340 October 2023
+
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
